585f319b58342ed3365a1bc6b3ec3f5658db0de6
Junel Solis <junel.solis@abo.fi>
2024-04-16 11:32:01 +0300

    incorporate manual inner zone rois

--- src/zoning_classes.py
+++ src/zoning_classes.py
@@ -1,15 +1,22 @@
 """
 Class definitions for zone segmentation
 """
+
+from typing import Optional
+import os
 import numpy as np
 import zarr
 from skimage import img_as_ubyte
+from skimage import io
 from skimage.draw import set_color
+from skimage.color import rgb2gray
 from skimage.exposure import adjust_gamma
 from skimage.morphology import dilation, disk, erosion
+from glob import glob
 
 INNER_ZONE_THICKNESS = 45
 OUTER_ZONE_THICKNESS = 45
+MANUAL_ROI_DIR = os.path.join(os.path.dirname(__file__), "..", "rois", "manual")
 
 
 def _generate_outer_zone(
@@ -139,6 +146,30 @@ class ApicalInZoner:
             }
         )
 
+        # If there is a mmanual roi for the inner zone, save it as well
+        inner_zone_manual = self._get_manual_inner_zone()
+
+        inner_zone_manual_dataset_path = (
+            f"{self.mix}/apical_in/{self.roi}/segmentation/zones/inner_manual"
+        )
+
+        if inner_zone_manual_dataset_path in self.root:
+            del self.root[inner_zone_manual_dataset_path]
+
+        if inner_zone_manual is not None:
+
+            inner_zone_manual_dataset = self.root.create_dataset(
+                inner_zone_manual_dataset_path, data=inner_zone_manual
+            )
+            inner_zone_manual_dataset.attrs.update(
+                {
+                    "author": "Turku BioImaging",
+                    "description": "Apical-in inner zone manual mask",
+                    "mix": self.mix,
+                    "roi": self.roi,
+                }
+            )
+
         if save_overlays is True:
             overlay_img = self._generate_overlays(outer_zone, inner_zone)
 
@@ -160,6 +191,19 @@ class ApicalInZoner:
                     "roi": self.roi,
                 }
             )
+
+    def _get_manual_inner_zone(self) -> Optional[np.ndarray]:
+        manual_inner_zone_paths = glob(os.path.join(MANUAL_ROI_DIR, "*.tif"))
+        manual_inner_zone_list = [
+            os.path.basename(item).replace(".tif", "")
+            for item in manual_inner_zone_paths
+        ]
+        
+        # print(manual_inner_zone_list)
+
+        if self.roi in manual_inner_zone_list:
+            manual_roi = io.imread(os.path.join(MANUAL_ROI_DIR, f"{self.roi}.tif"))
+            return rgb2gray(manual_roi) > 0
 
     def _generate_overlays(
         self, outer_zone: np.ndarray, inner_zone: np.ndarray, alpha=0.25
